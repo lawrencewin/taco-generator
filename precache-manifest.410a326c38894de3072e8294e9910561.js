89dcacf028d8d53eeeb525f18a05830a9f16ce16
dehunter456 <dehunter456@gmail.com>
2019-10-02 05:54:13 -0500

    Updates

---
+++ precache-manifest.410a326c38894de3072e8294e9910561.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c55f58198a44516efb8949ae2c01dc7",
+    "url": "/taco-generator/index.html"
+  },
+  {
+    "revision": "22cec730afff957c3822",
+    "url": "/taco-generator/static/css/main.0c8c8ba4.chunk.css"
+  },
+  {
+    "revision": "4763957812034c049db6",
+    "url": "/taco-generator/static/js/2.2e054e2e.chunk.js"
+  },
+  {
+    "revision": "22cec730afff957c3822",
+    "url": "/taco-generator/static/js/main.082cacdc.chunk.js"
+  },
+  {
+    "revision": "a80dedbd2881ce860073",
+    "url": "/taco-generator/static/js/runtime-main.9cd63d12.js"
+  }
+]);
