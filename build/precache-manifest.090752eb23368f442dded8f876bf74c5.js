c405c5cf68a2d71c4e41527b0a505fe614ac2b0a
dehunter456 <dehunter456@gmail.com>
2019-10-02 05:31:33 -0500

    pushing build folder

---
+++ build/precache-manifest.090752eb23368f442dded8f876bf74c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf72ae2e5b6e473cee16388954a2c05c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c75542e913f3e91a097b",
+    "url": "/static/css/main.0c8c8ba4.chunk.css"
+  },
+  {
+    "revision": "25fd1ff06d8124f08199",
+    "url": "/static/js/2.fc1a50d5.chunk.js"
+  },
+  {
+    "revision": "c75542e913f3e91a097b",
+    "url": "/static/js/main.3a1762d3.chunk.js"
+  },
+  {
+    "revision": "b69aca92f325cc8c5c5e",
+    "url": "/static/js/runtime-main.cb91dccb.js"
+  }
+]);
